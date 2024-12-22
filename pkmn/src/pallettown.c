2bddf8001e5d43a916cd4bec978f3fd587a99d1d
Matt 'Smartis' W <matt.weingaertner@gmail.com>
2024-12-22 22:36:41 +0100

    Fix bkg_tiles loading by dividing by 8

--- pkmn/src/pallettown.c
+++ pkmn/src/pallettown.c
@@ -7,5 +7,6 @@ void ShowPalletTown()
     SHOW_BKG;
 
     set_bkg_data(0, tileset_0_TILE_COUNT, tileset_0_tiles);
-    set_bkg_tiles(0, 0, pallettown_map_WIDTH, pallettown_map_HEIGHT, pallettown_map_map);
+    // *_WIDHT and *_HEIGHT are in pixels, so we need to divide by 8 to get the number of tiles
+    set_bkg_tiles(0, 0, pallettown_map_WIDTH >> 3, pallettown_map_HEIGHT >> 3, pallettown_map_map);
 }
