6190e89dd7c75e5f51c239130e55a3de3ccb1191
Matt 'Smartis' W <matt.weingaertner@gmail.com>
2024-12-22 22:37:28 +0100

    adding ToDo

--- pkmn/src/pallettown.c
+++ pkmn/src/pallettown.c
@@ -9,4 +9,6 @@ void ShowPalletTown()
     set_bkg_data(0, tileset_0_TILE_COUNT, tileset_0_tiles);
     // *_WIDHT and *_HEIGHT are in pixels, so we need to divide by 8 to get the number of tiles
     set_bkg_tiles(0, 0, pallettown_map_WIDTH >> 3, pallettown_map_HEIGHT >> 3, pallettown_map_map);
+
+    // ToDo: Load in Chunks beacuse of the 256x256 tile limit
 }
